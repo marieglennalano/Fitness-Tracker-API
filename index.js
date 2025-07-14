6841161382acd3e923aa1265c03b0b884131898d
Marie Glenn Alano <marieglennalano@gmail.com>
2025-07-15 07:51:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 // Allow all resources
 const corsOptions = {
     //to be updated when we connect this to our client
-    origin: ['https://fitness-tracker-api-enez.onrender.com/'],
+    origin: ['https://fitness-tracker-fs.vercel.app/'],
     credentials: true,
     optionsSuccessStatus: 200
 };
